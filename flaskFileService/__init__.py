1c7cc75ae7d06d029e4bf4717e412ca8caffcf9e
clayton <clayton@claytondunavant.com>
2021-01-23 15:59:45 -0500

    trying to get correct css

--- flaskFileService/__init__.py
+++ flaskFileService/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_file, send_from_directory
 
+
 app = Flask(__name__)
 
 DOWNLOAD_DIRECTORY = "get-files"
@@ -10,8 +11,6 @@ def get_files(path):
         return send_from_directory(DOWNLOAD_DIRECTORY, path, as_attachment=True)
     except FileNotFoundError:
         return False
-
-#app.run(debug = True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 8000, threaded = True, debug = True)
