faa6fc16c991e07819726da7e86f494e6dcd2486
Bilsha <bmohanda@purdue.edu>
2021-01-23 13:06:11 -0500

    commentbad py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	flaskFileService.get_files('get-files')
+	#flaskFileService.get_files('get-files')
 	return render_template('home.html')
 '''
 @app.route('/upload/')
