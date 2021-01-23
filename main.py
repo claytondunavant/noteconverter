48e5b8eedfd865340b06ec929a5aabb233a49817
Alec <agestenslager@gmail.com>
2021-01-22 23:25:07 -0500

    changed method 'index' to 'home'; created frame

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
 	return render_template('home.html')
+'''
+@app.route('/upload/')
+def upload():
+	return render_template('upload.html')'''
 
 if __name__ == '__main__':
 	app.run(debug=True)
