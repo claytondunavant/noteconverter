2341b7e144ee144d6bf03af4bddd366820a90dd6
Alec <agestenslager@gmail.com>
2021-01-23 12:50:44 -0500

    added flaskFileService

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+import flaskFileService
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+	flaskFileService.get_files('get-files')
 	return render_template('home.html')
 '''
 @app.route('/upload/')
---
+++ flaskFileService.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, send_file, send_from_directory
+
+app = Flask(__name__)
+
+DOWNLOAD_DIRECTORY = "get-files"
+
+@app.route('/get-files/<path:path>', methods = ['GET', 'POST'])
+def get_files(path):
+    try:
+        return send_from_directory(DOWNLOAD_DIRECTORY, path, as_attachment=True)
+    except FileNotFoundError:
+        return False
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 8000, threaded = True, debug = True)
