6b7b463a1428d7c7274a345486f43ca7d7b3a561
Alec <agestenslager@gmail.com>
2021-01-22 22:55:54 -0500

    changed template to templates

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('Home.html')
-
-@app.route('/my-link/')
-def my_link():
-	print("I get clicked!")
-	
-	return 'Click.'
+	return render_template('home.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
